71afc6b13b4a18c2b5ed861f55d54dd567675f89
my0218 <124230467+my0218@users.noreply.github.com>
2023-05-28 18:49:58 -0400

    version 2

--- Starter_Code/StarterCode/static/js/app.js
+++ Starter_Code/StarterCode/static/js/app.js
@@ -6,7 +6,9 @@ d3.json(url).then(function(data) {
   let samples = data.samples;
   let dropdown = d3.select("#selDataset");
   let sampleIds = samples.map(sample => sample.id);
-  let chart = d3.select("#bar");
+  let barchart = d3.select("#bar");
+  let bubblechart = d3.select ("#col-md-12")
+  let demographicinfo = d3.select ("#sample-metadata")
 
   dropdown
     .selectAll("option")
@@ -30,29 +32,75 @@ d3.json(url).then(function(data) {
         };
       }
 
-  function displayChart(selectedIndex) {
+  function barChart(selectedIndex) {
     let selectedSample = samples.find(sample => sample.id === sampleIds[selectedIndex]);
     let { otuIds, sampleValues, otuLabels } = top10otuvalue(selectedSample);
-    let trace = {
+    let trace1 = {
       x: sampleValues,
       y: otuIds,
       type: 'bar',
-      orientation: 'h',
+      orientation: 'h'
     };
-    let data = [trace];
-    let layout = {
-      margin: { t: 5, r: 5, b: 20, l: 100 }, 
+    let data1 = [trace1];
+    let layout1 = {
+      title: 'Top 10 OTUs',
+      margin: { t: 50, r: 5, b: 20, l: 100 }, 
       height: 300 
     };
-    Plotly.newPlot('bar', data, layout);
+    Plotly.newPlot('bar', data1, layout1);
   }
 
-  displayChart(0);
+  barChart(0);
+
+  function bubbleChart(selectedIndex) {
+    let selectedSample = samples.find(sample => sample.id === sampleIds[selectedIndex]);
+
+    let trace2 = {
+      x: selectedSample.otu_ids,
+      y: selectedSample.sample_values,
+      text: selectedSample.otu_labels,
+      mode: 'markers',
+      marker: {
+        size: selectedSample.sample_values,
+        color: selectedSample.otu_ids,
+        colorscale: 'Earth'
+      }
+    };
+
+    let data2 = [trace2];
+
+    let layout2 = {
+      margin: { t: 20, r: 5, b: 30, l: 100 },
+      height: 400,
+      xaxis: {
+        title: 'OTU IDs'
+      },
+      yaxis: {
+        title: 'Sample Values'
+      }
+    };
+
+    Plotly.newPlot('bubble', data2, layout2);
+  }
+
+  bubbleChart(0);
+
+  function SampleMetadata(selectedIndex) {
+    let selectedMetadata = data.metadata[selectedIndex];
+    demographicinfo.html("");
+    Object.entries(selectedMetadata).forEach(([key, value]) => {
+        demographicinfo.append("p").text(`${key}: ${value}`);
+    });
+  }
+
+  SampleMetadata(0);
 
   function optionChanged(selectedIndex) {
-    displayChart(selectedIndex);
+    barChart(selectedIndex);
+    bubbleChart(selectedIndex);
+    SampleMetadata(selectedIndex);
   }
-  
+
   dropdown.on("change", function() {
     let selectedIndex = +this.value;
     optionChanged(selectedIndex);
